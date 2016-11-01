43294c7a5fbcb1992d5288714273f6f3785a1d57
George Olson <george.olson@windowslive.com>
2016-11-01 10:37:05 -0700

    no message

--- Framework/Slalom.Boost.DocumentDb/Properties/AssemblyInfo.cs
+++ Framework/Slalom.Boost.DocumentDb/Properties/AssemblyInfo.cs
@@ -1,36 +1,20 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Slalom.Boost.DocumentDb")]
-[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyTitle("Slalom Boost - DocumentDB")]
+[assembly: AssemblyDescription("Slalom Boost - DocumentDB Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Slalom.Boost.DocumentDb")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("961850ab-1cb3-4dd0-b45f-fb26e50aba48")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("23e98f1f-09fd-48b8-a22d-77e2106487fd")]
